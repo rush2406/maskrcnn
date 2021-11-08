b5b3f874d5f05cb9aa9b6572feefe581c1a78e9d
rush2406 <rushaligrandhe@gmail.com>
2021-11-08 18:22:09 +0400

    First commit

---
+++ Detectron2_Mask-RCNN_iSAID.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Tutorial adapted from the Detectron2 colab example
+
+# # Install detectron2
+# https://github.com/facebookresearch/detectron2
+# 
+# https://detectron2.readthedocs.io/en/latest/tutorials/install.html
+
+# In[1]:
+
+
+# check pytorch installation: 
+import torch, torchvision
+print(torch.__version__, torch.cuda.is_available())
+
+
+# In[2]:
+
+
+# Some basic setup:
+# Setup detectron2 logger
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import numpy as np
+import os, json, cv2, random
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+import wandb
+
+
+# # Train on a iSAID dataset
+
+# In this section, we show how to train an existing detectron2 model on the iSAID dataset.
+# 
+# 
+# ## Prepare the dataset
+
+# Since iSAID is in COCO format, it can be easily registered in Detectron2
+
+# In[3]:
+
+
+
+# register_coco_instances("my_dataset_train", {}, "json_annotation_train.json", "path/to/image/dir")
+# register_coco_instances("my_dataset_val", {}, "json_annotation_val.json", "path/to/image/dir")
+
+wandb.login()
+wandb.init(project='maskrcnn',sync_tensorboard=True,settings=wandb.Settings(start_method="thread", console="off"))
+
+from detectron2.data.datasets import register_coco_instances
+register_coco_instances("iSAID_train", {}, 
+                        "/apps/local/shared/CV703/datasets/iSAID/iSAID_patches/train/instancesonly_filtered_train.json",
+                        "/apps/local/shared/CV703/datasets/iSAID/iSAID_patches/train/images/")
+register_coco_instances("iSAID_val", {}, 
+                        "/apps/local/shared/CV703/datasets/iSAID/iSAID_patches/val/instancesonly_filtered_val.json",
+                        "/apps/local/shared/CV703/datasets/iSAID/iSAID_patches/val/images/")
+
+
+# ## Train!
+# 
+# Now, let's fine-tune a COCO-pretrained R101-FPN Mask R-CNN model on the iSAID dataset.
+
+# In[2]:
+
+
+from detectron2.engine import DefaultTrainer
+
+cfg = get_cfg()
+cfg.OUTPUT_DIR = 'output_maskrcnn_imagenet'
+cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml"))
+cfg.DATASETS.TRAIN = ("iSAID_train",)
+cfg.DATASETS.TEST = ()
+cfg.DATALOADER.NUM_WORKERS = 2
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml")  # Let training initialize from model zoo
+cfg.SOLVER.IMS_PER_BATCH = 2
+cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+cfg.SOLVER.MAX_ITER = 100000#
+cfg.SOLVER.STEPS = []        # do not decay learning rate
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster (default: 512)
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 15  
+
+
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+trainer = DefaultTrainer(cfg) 
+trainer.resume_or_load(resume=False)
+trainer.train()
+
+
+# ### Look at training curves in tensorboard by running in the terminal:
+# tensorboard --logdir output_maskrcnn
+
+# ## Inference & evaluation using the trained model
+# Now, let's run inference with the trained model on the validation dataset. First, let's create a predictor using the model we just trained:
+# 
+# 
+
+# In[9]:
+
+
+# Inference should use the config with parameters that are used in training
+# cfg now already contains everything we've set previously. We changed it a little bit for inference:
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
+predictor = DefaultPredictor(cfg)
+
+
+# In[1]:
+
+
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+from detectron2.data import build_detection_test_loader
+evaluator = COCOEvaluator("iSAID_val")
+val_loader = build_detection_test_loader(cfg, "iSAID_val")
+print(inference_on_dataset(trainer.model, val_loader, evaluator))
+# another equivalent way to evaluate the model is to use `trainer.test`
+
+
+# In[ ]:
+
+
+
+
